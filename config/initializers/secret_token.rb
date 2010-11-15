8575bf209b03eb9e0e26e6d3fbbe2f6ac85022fb
Josh Starcher <josh.starcher@gmail.com>
2010-11-15 11:52:37 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mi::Application.config.secret_token = 'abfd5216b01e96e116b2341a1532757233068067e7ac7fc2aa03015f8ba59fee2a54bb53028434fcf8de3102cae14dd3193d8c30f1554e758d55bcfd01b49b9a'
