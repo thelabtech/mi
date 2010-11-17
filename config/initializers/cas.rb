c9cae9520ea5cd6fb2385e80ae461611104ebbea
Twinge <josh.starcher@gmail.com>
2010-11-16 21:17:00 -0500

    memcached for sessions

--- config/initializers/cas.rb
+++ config/initializers/cas.rb
@@ -1,5 +1,6 @@
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
 CASClient::Frameworks::Rails::Filter.configure(
-  :cas_base_url => "https://signin.ccci.org/sso"
+  :cas_base_url => "https://signin.ccci.org/cas",
+  :extra_attributes_session_key => :cas_extra_attributes
 )
