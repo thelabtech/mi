2f1a8b6a62d26b0f8b099e6ac22a4557216364e8
Josh Starcher <josh.starcher@gmail.com>
2010-11-17 09:54:23 -0500

    add back emplid check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   
   protected
   def staff_required
-    unless session[:cas_extra_attributes] #&& session[:cas_extra_attributes]['emplid'].present?
+    unless session[:cas_extra_attributes] && session[:cas_extra_attributes]['emplid'].present?
       redirect_to '/dashboard/not_staff' and return false
     end
   end
